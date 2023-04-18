36b3415bf8e6573133c381680bb5e7065298cdcd
andresmr <andres@andresmr.com>
2023-04-18 14:55:36 +0200

    Add login ui

--- common/src/commonMain/kotlin/com/andresmr/common/App.kt
+++ common/src/commonMain/kotlin/com/andresmr/common/App.kt
@@ -1,21 +1,12 @@
 package com.andresmr.common
 
-import androidx.compose.material.Text
-import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import com.andresmr.common.ui.LoginScreen
 
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-    val platformName = getPlatformName()
-
-    Button(onClick = {
-        text = "Hello, ${platformName}"
-    }) {
-        Text(text)
+    MaterialTheme {
+        LoginScreen { email, password ->  }
     }
 }
---
+++ common/src/commonMain/kotlin/com/andresmr/common/ui/LoginScreen.kt
@@ -0,0 +1,44 @@
+package com.andresmr.common.ui
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+
+@Composable
+fun LoginScreen(onLogin: (String, String) -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Login Screen", style = MaterialTheme.typography.h4)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { onLogin(email, password) },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
